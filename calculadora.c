32eb8719daf46e562e9f2103e267edae90946029
maxifes <acasmax@comunidad.unam.mx>
2022-12-02 14:24:54 -0600

    HaciendoCommitInicial

---
+++ calculadora.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+
+float calcula_Perimetro_Cuadrado(float a);
+float calcula_Perimetro_Rectangulo(float a,float b); 
+float calcula_Perimetro_Triangulo(float a,float b, float c);
+float calcula_Circunferencia(float r);  
+float calcula_Area_Cuadrado(float a); 
+float calcula_Area_Rectangulo(float a, float b);
+float calcula_Area_Triangulo(float base, float altura); 
+float calcula_Area_Circulo(float r);
+float suma_N_Numeros(float *arreglo, int sizeArreglo);
+float multiplica_N_Numeros(float *arreglod, float sizeArreglo); 
+float elevar_aPotencia(float base,float potencia);
+
+
+
+int main(int argc, char const *argv[])
+{
+    float a;
+
+    printf("%f",a);
+    return 0;
+}
