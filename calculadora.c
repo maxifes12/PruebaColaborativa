f2e070d2f2e865f7ed80a434e5a0d2b1bde8b7ca
maxifes12 <maximiliano_fragoso@outlook.com>
2022-12-02 14:32:39 -0600

    Haciendo segundo commit

--- calculadora.c
+++ calculadora.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdint.h>
 
-float calcula_Perimetro_Cuadrado(float a);
-float calcula_Perimetro_Rectangulo(float a,float b); 
-float calcula_Perimetro_Triangulo(float a,float b, float c);
-float calcula_Circunferencia(float r);  
-float calcula_Area_Cuadrado(float a); 
-float calcula_Area_Rectangulo(float a, float b);
-float calcula_Area_Triangulo(float base, float altura); 
-float calcula_Area_Circulo(float r);
-float suma_N_Numeros(float *arreglo, int sizeArreglo);
-float multiplica_N_Numeros(float *arreglod, float sizeArreglo); 
-float elevar_aPotencia(float base,float potencia);
+float calcula_Perimetro_Cuadrado(float a); //1  
+float calcula_Perimetro_Rectangulo(float a,float b); //2 
+float calcula_Perimetro_Triangulo(float a,float b, float c); //3
+float calcula_Circunferencia(float r); //4
+float calcula_Area_Cuadrado(float a); //5
+float calcula_Area_Rectangulo(float a, float b); //6
+float calcula_Area_Triangulo(float base, float altura); //7 
+float calcula_Area_Circulo(float r); //8
+float suma_N_Numeros(float *arreglo, int sizeArreglo); //9
+float multiplica_N_Numeros(float *arreglod, float sizeArreglo); //10 
+float elevar_aPotencia(float base,float potencia); //11
 
 
 
